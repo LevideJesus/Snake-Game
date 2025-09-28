6ab7dc51820fcf59cc96e56b9dcc9a36c1ea8a56
Levi de Jesus <levijesus2121@gmail.com>
2025-09-28 20:28:31 -0300

    drawing the snake on the screen

--- snake-game.py
+++ snake-game.py
@@ -3,9 +3,16 @@ from pygame.locals import *
 
 WINDOWS_WIDTH = 600
 WINDOWS_HEIGHT = 600
+BLOCK = 10
+PLACE_X = WINDOWS_WIDTH / 2
+PLACE_Y = WINDOWS_HEIGHT / 2
 
 pygame.init() #system already to use the window
 window = pygame.display.set_mode((WINDOWS_WIDTH, WINDOWS_HEIGHT))
+
+snake_pos = [(PLACE_X, PLACE_Y)]
+snake_surface = pygame.Surface((BLOCK,BLOCK))
+snake_surface.fill((53,59,72))
 
 while True: # to keep the window opens.
   window.fill((68,189,50))
@@ -14,3 +21,8 @@ while True: # to keep the window opens.
       if event.type == QUIT:
          pygame.quit()
          quit()
+
+  for place in snake_pos:
+    window.blit(snake_surface,place) # blit to draw something on the screen. 
+
+  pygame.display.update() # to update the screen 
